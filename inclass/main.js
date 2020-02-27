9fb639684318f7e762e452fb39522c7d3e8a00be
Jade Chen <jadechen@Jade.cable.rcn.com>
2020-02-27 00:36:41 -0500

    studio

--- inclass/main.js
+++ inclass/main.js
@@ -6,3 +6,28 @@ function printWord (word) {
 
 printWord('iiiiiiii');
 printWord("oooooo");
+
+function changeHeading() {
+	document.getElementbyId('Heading').innerHTML = "I clicked the button"
+}
+
+function changeHeading() {
+	document.getElementbyId('heading').style.backgroundColor ="pink"
+}
+
+function changeHeading () {
+	document.getElementById('heading').innerHTML="i clicked the button"
+}
+
+function changeHeading () {
+	document.getElementById('heading').innerHTML="i clicked the button"
+}
+
+function changeHeading(color) {
+	document.getElementById('heading').innerHTML=color;
+}
+
+function changeSubHeading(fontcolor) {
+	document.getElementById('subheading').style.color=fontcolor;
+}
+
